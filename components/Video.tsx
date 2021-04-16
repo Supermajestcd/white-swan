e3ab4aaa09c702358cf7d0bd0176062026205651
Dirk de Klerk <dirk.deklerk@blackswan.com>
2021-04-16 16:23:45 +0200

    Added video poster

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,7 +4,8 @@ import { GridItem } from '@chakra-ui/layout'
 interface Props {
   index: number
   data: {
-    videoLink: string
+    videoLink: string;
+    posterImageName: string;
   }
 }
 
@@ -12,7 +13,7 @@ export const Video = ({ index, data }: Props) => {
   return (
     <GridItem key={index} gridColumn={'2 / 14'}>
       <BlocksControls index={index} insetControls label={false}>
-          <video controls style={{ margin: '30px auto' }}>
+          <video controls style={{ margin: '30px auto' }} poster={`/${data.posterImageName}`}>
             <source src={`/${data.videoLink}`} type='video/mp4'/>
           </video>
       </BlocksControls>
@@ -27,6 +28,11 @@ export const VideoTemplate = {
       component: 'text',
       name: 'videoLink',
       label: 'Video filename which has been added to the media Manager'
+    },
+    {
+      component: 'text',
+      name: 'posterImageName',
+      label: 'The title of the uploaded image to be used as the video poster image'
     }
   ]
 }
