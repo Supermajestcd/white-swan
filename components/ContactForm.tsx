18db41297b1e9680709c660cff47b79ebb3388b1
Dirk de Klerk <dirk.deklerk@blackswan.com>
2022-08-04 07:54:41 +0200

    Added new contact us form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,45 +1,56 @@
-import { BlocksControls } from 'react-tinacms-inline'
+// import { BlocksControls } from 'react-tinacms-inline'
+import { useHubspotForm } from '@aaronhayes/react-use-hubspot-form'
 import { GridItem } from '@chakra-ui/layout'
-import { FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react'
+// import { FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react'
 
 interface Props {
   index: number
 }
 
 export const Form = ({ index }: Props) => {
+  const portalId = '4905990'
+  const formId = '316b958f-2269-41f2-9a7d-c5b8dbebf07e'
+
+  useHubspotForm({
+    portalId,
+    formId,
+    target: '#hubspotSocialDiv'
+  })
+
   return (
     <GridItem key={index} gridColumn={'5/span 6'}>
-      <BlocksControls index={index} insetControls label={false}>
-        <form
-          name="contact"
-          method="post"
-          action="/contact-thank-you/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:
-              <input name="bot-field"/>
-            </label>
-          </p>
-          <FormControl id="name" p={'3'}>
-            <FormLabel>Name</FormLabel>
-            <Input type="text" name="name" required/>
-          </FormControl>
-          <FormControl id="email" p={'3'}>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" name="email" required/>
-          </FormControl>
-          <FormControl id="comment" p={'3'}>
-            <FormLabel>Comment</FormLabel>
-            <Textarea type="comment" name="comment" required/>
-          </FormControl>
-          <Button type="submit" m={'3'}>Send</Button>
-        </form>
-      </BlocksControls>
+      <div><div id="hubspotSocialDiv" style={{ padding: '2.5em', paddingTop: '0' }} /></div>
+      {/* <BlocksControls index={index} insetControls label={false}> */}
+      {/*  <form */}
+      {/*    name="contact" */}
+      {/*    method="post" */}
+      {/*    action="/contact-thank-you/" */}
+      {/*    data-netlify="true" */}
+      {/*    data-netlify-honeypot="bot-field" */}
+      {/*  > */}
+      {/*    /!* The `form-name` hidden field is required to support form submissions without JavaScript *!/ */}
+      {/*    <input type="hidden" name="form-name" value="contact" /> */}
+      {/*    <p hidden> */}
+      {/*      <label> */}
+      {/*        Don’t fill this out: */}
+      {/*        <input name="bot-field"/> */}
+      {/*      </label> */}
+      {/*    </p> */}
+      {/*    <FormControl id="name" p={'3'}> */}
+      {/*      <FormLabel>Name EDIT</FormLabel> */}
+      {/*      <Input type="text" name="name" required/> */}
+      {/*    </FormControl> */}
+      {/*    <FormControl id="email" p={'3'}> */}
+      {/*      <FormLabel>Email</FormLabel> */}
+      {/*      <Input type="email" name="email" required/> */}
+      {/*    </FormControl> */}
+      {/*    <FormControl id="comment" p={'3'}> */}
+      {/*      <FormLabel>Comment</FormLabel> */}
+      {/*      <Textarea type="comment" name="comment" required/> */}
+      {/*    </FormControl> */}
+      {/*    <Button type="submit" m={'3'}>Send</Button> */}
+      {/*  </form> */}
+      {/* </BlocksControls> */}
     </GridItem>
   )
 }
--- utils/useHubspotFormDefs.tsx
+++ utils/useHubspotFormDefs.tsx
@@ -9,10 +9,11 @@ export const generateForm = (filename: string) => {
     'improving-diagnosis-and-treatment-for-back-pain.json': { portalId: '4905990', formId: '52422817-36fb-4841-806d-3240509a7576' },
     'supporting-parkinsons-sufferers.json': { portalId: '4905990', formId: '4a862623-4411-45b6-9ef5-0a5980dc2703' },
     'analysing-fibromyalgia-needs.json': { portalId: '4905990', formId: 'bbabac64-a984-48dc-ab23-266ef7f332e8' }
+    // 'contact.json': { portalId: '4905990', formId: '316b958f-2269-41f2-9a7d-c5b8dbebf07e' }
   })
 
   if (formDefs.current[filename]) {
-    const { portalId, formId } = formDefs.current[filename];
+    const { portalId, formId } = formDefs.current[filename]
     useHubspotForm({
       portalId,
       formId,
